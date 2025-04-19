9df5bf51d83968d5893ef734f946e7659a7db288
kayarhabby <pasciahershekaya@gmail.com>
2025-04-20 00:53:20 +0200

    inclusion du modèle Team dans admin

--- RestApi/admin.py
+++ RestApi/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Register your models here.
+from RestApi.models import Team
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+
+    # Display image in admin panel
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="100"/>')
+        return "-"
+    image_tag.short_description = 'Image'
+
+admin.site.register(Team, TeamAdmin)
