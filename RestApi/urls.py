9cec92694836c74468e23225498b37d634b38d3f
kayarhabby <pasciahershekaya@gmail.com>
2025-04-20 11:59:53 +0200

    update urls

--- RestApi/urls.py
+++ RestApi/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import IndexTemplateView
 
 app_name = 'RestApi'
 
 urlpatterns = [
-
+    path('', IndexTemplateView.as_view(), name='index'),
+    path('team/', views.TeamViewSet.as_view({'get': 'list'}), name='team-list'),
 ]
