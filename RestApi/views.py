3f6acd03a0a56974d4f1b3c755d3ae18e2f48a40
kayarhabby <pasciahershekaya@gmail.com>
2025-04-20 12:00:11 +0200

    update views

--- RestApi/views.py
+++ RestApi/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Team
+from .serializers import TeamSerializer
+
+# Create your views here.
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class IndexTemplateView(TemplateView):
+    template_name = 'index.html'
