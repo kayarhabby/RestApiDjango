9e70a1d3775a882538169fb760ace9fba27d9c84
kayarhabby <pasciahershekaya@gmail.com>
2025-04-20 12:56:22 +0200

    mise en place de la route api servant team

---
+++ RestApi/urls_api.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import TeamViewSet
+
+router = DefaultRouter()
+router.register(r'team', TeamViewSet, basename='team')
+
+urlpatterns = router.urls
